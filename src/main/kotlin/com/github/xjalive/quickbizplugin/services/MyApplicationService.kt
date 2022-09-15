8eb7388776981e466d7fab8a9267095b56ce74f1
GitHub Action <action@github.com>
2022-09-15 08:53:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/xjalive/quickbizplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xjalive/quickbizplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xjalive.quickbizplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xjalive.quickbizplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/xjalive/quickbizplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/xjalive/quickbizplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.xjalive.quickbizplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xjalive.quickbizplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
